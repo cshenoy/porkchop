7ce93f292e5b13a616e0b9634436daad9c9732a6
Chetan Shenoy <me@chetanshenoy.com>
2018-08-13 23:24:38 -0400

    fix reference to get admin email and add get user fn

--- lib/porkchop/administration/administration.ex
+++ lib/porkchop/administration/administration.ex
@@ -1,14 +1,24 @@
 defmodule Porkchop.Administration do
-  alias Mango.Repo
+  alias Porkchop.Repo
+  alias Porkchop.Administration.User
 
-  alias Mango.Administration.User
+  @doc """
+  Gets a single user.
+  Raises `Ecto.NoResultsError` if the User does not exist.
+  ## Examples
+      iex> get_user!(123)
+      %User{}
+      iex> get_user!(456)
+      ** (Ecto.NoResultsError)
+  """
+  def get_user!(id), do: Repo.get!(User, id)
 
   def get_admin_by_email(email) do
     User |> Repo.get_by(email: email)
   end
 
   def get_admin_by_credentials(%{"email" => email, "password" => pass}) do
-    customer = get_customer_by_email(email)
+    customer = get_admin_by_email(email)
     cond do
       customer && Comeonin.Bcrypt.checkpw(pass, customer.password_hash) ->
         customer
