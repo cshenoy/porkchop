c566ddaea3dbf0d25095f78b7958e64b0416f4b8
Chetan Shenoy <me@chetanshenoy.com>
2018-08-16 09:41:47 -0400

    fix reference to Author entity

--- lib/porkchop/administration/administration.ex
+++ lib/porkchop/administration/administration.ex
@@ -1,20 +1,20 @@
 defmodule Porkchop.Administration do
   alias Porkchop.Repo
-  alias Porkchop.Administration.User
+  alias Porkchop.Administration.Author
 
   @doc """
-  Gets a single user.
-  Raises `Ecto.NoResultsError` if the User does not exist.
+  Gets a single Author.
+  Raises `Ecto.NoResultsError` if the Author does not exist.
   ## Examples
       iex> get_user!(123)
-      %User{}
+      %Author{}
       iex> get_user!(456)
       ** (Ecto.NoResultsError)
   """
-  def get_user!(id), do: Repo.get!(User, id)
+  def get_user!(id), do: Repo.get!(Author, id)
 
   def get_admin_by_email(email) do
-    User |> Repo.get_by(email: email)
+    Author |> Repo.get_by(email: email)
   end
 
   def get_admin_by_credentials(%{"email" => email, "password" => pass}) do
